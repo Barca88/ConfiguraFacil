cdf3e05d5e07089bcf2e3e545cb7dd63ed7687b9
Marco Gonçalves <marcomatiaspg@gmail.com>
2018-12-18 23:26:46 +0000

    start dao

---
+++ code/src/configuraFacil/dataBase/PacoteDao.java
@@ -0,0 +1,109 @@
+package configuraFacil.dataBase;
+
+import configuraFacil.business.models.Pacote;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.*;
+
+public class PacoteDao implements Map<Integer, Pacote> {
+
+    private Connection conn;
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+    @Override
+    public boolean containsKey(Object key){
+        boolean ret = false;
+        try {
+            conn = Connect.connect();
+            String sql = "SELECT 'nome' FROM 'Pacote' WHERE nome=?";
+            PreparedStatement stm = conn.prepareStatement(sql);
+            stm.setString(1,(String)key);
+            ResultSet rs = stm.executeQuery();
+            ret = rs.next();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            Connect.close(conn);
+        }
+        return ret;
+    }
+
+    @Override
+    public boolean containsValue(Object o) {
+        return false;
+    }
+
+    @Override
+    public Pacote get(Object o) {
+        return null;
+    }
+
+    @Override
+    public Pacote put(Integer integer, Pacote pacote) {
+        return null;
+    }
+
+    @Override
+    public Pacote remove(Object o) {
+        return null;
+    }
+
+    @Override
+    public void putAll(Map<? extends Integer, ? extends Pacote> map) {
+
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public Set<Integer> keySet() {
+        return null;
+    }
+
+    @Override
+    public Collection<Pacote> values() { //Todo Shaman
+        List<Pacote> ret = new ArrayList<>();
+        try {
+            conn = Connect.connect();
+            String sql = "SELECT * FROM 'Pacote' sortby 'id'" + "inner join 'Pacote_has_Item' on 'Pacote.idPacote'='Pacote_has_Item.'\n" + "inerjoin 'Item'\n" + "where Pacote_idPacote=?";
+            PreparedStatement stm = conn.prepareStatement(sql);
+            ResultSet rs = stm.executeQuery();
+            while(rs.next()){
+                p = new Pacote(rs.getInt("id"),
+                               rs.getFloat("desconto"),
+                               rs.getString("nome"))
+
+                ret.add(p);
+            }
+            sql = "SELECT * FROM 'Pacote_has_Item' sortby 'Pacote_idPacote'";
+            PreparedStatement stm = conn.prepareStatement(sql);
+            ResultSet rs = stm.executeQuery();
+            int i = 0;
+            while(rs.next())
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            Connect.close(conn);
+        }
+        return ret;
+    }
+
+    @Override
+    public Set<Entry<Integer, Pacote>> entrySet() {
+        return null;
+    }
+
+}
