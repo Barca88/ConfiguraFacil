a8fb13b7e34cf96a3630c96af6fd5e247ead674c
Marco Gonçalves <marcomatiaspg@gmail.com>
2018-12-20 14:56:25 +0000

    start Stock Fabrica controller

---
+++ code/src/configuraFacil/presentation/controllers/StockFabricaController.java
@@ -0,0 +1,60 @@
+package configuraFacil.presentation.controllers;
+
+import configuraFacil.business.ConfiguraFacil;
+import configuraFacil.business.models.items.Item;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.Hashtable;
+import java.util.Map;
+
+public class StockFabricaController {
+
+    private ConfiguraFacil cf;
+
+    private Map<Integer,Integer> encomenda;
+
+    @FXML
+    private TableView<Item> tblStockFabricante;
+    @FXML
+    private TableColumn<Item,Integer> clnIdStock;
+    @FXML
+    private TableColumn<Item,Integer> clnNomeStock;
+    @FXML
+    private TableColumn<Item,Integer> clnTipoStock;
+    @FXML
+    private TableColumn<Item,String> clnStockStock;
+
+    public void init(ConfiguraFacil cfo) {
+        cf = cfo;
+        encomenda = new Hashtable<>();
+        initTable();
+    }
+
+    private void initTable(){
+        clnIdStock.setCellValueFactory(new PropertyValueFactory<>("id"));
+        clnNomeStock.setCellValueFactory(new PropertyValueFactory<>("nome"));
+        clnTipoStock.setCellValueFactory(new PropertyValueFactory<>("tipo"));
+        clnStockStock.setCellValueFactory(new PropertyValueFactory<>("stock"));
+        //tblStockFabricante.setItems(cf.getConfiguracoes());
+    }
+
+    public void handleBtnRecebeEncomenda(){return;}
+
+    public void handleBtnBack(ActionEvent actionEvent) throws IOException {
+
+        URL url =  getClass().getResource("../views/fabricante.fxml");
+        Stage window = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        SceneManager sm = new SceneManager(url, window);
+        sm.newScene(2, cf);
+
+    }
+}
