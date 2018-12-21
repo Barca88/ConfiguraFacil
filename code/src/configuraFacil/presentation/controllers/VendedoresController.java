62def3b6d503565e04b7fc7e013550bbb39d0e1a
tymoshchuk19 <ricardocanela.ns@gmail.com>
2018-12-21 00:13:27 +0000

    view vendedores

---
+++ code/src/configuraFacil/presentation/controllers/VendedoresController.java
@@ -0,0 +1,14 @@
+package configuraFacil.presentation.controllers;
+
+import configuraFacil.business.ConfiguraFacil;
+
+public class VendedoresController {
+
+    ConfiguraFacil cf;
+
+    public void init(ConfiguraFacil cfo) {
+        cf = cfo;
+    }
+
+
+}
